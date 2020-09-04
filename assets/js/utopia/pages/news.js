3fee53060b23a9a7cef1f6ff2c73b50e994ac10c
eric2788 <tsukiko852@gmail.com>
2020-09-05 02:02:20 +0800

    fix news and web-socket

--- assets/js/utopia/pages/news.js
+++ assets/js/utopia/pages/news.js
@@ -18,14 +18,14 @@ fetch('./settings/news.json').then(r=>r.json()).then(json => {
 }).catch(handleErrorAlert)
 
 
-function appendList(...news){
+function appendList(news) {
     const list = $('#news-list')
-    for(const {id, icon, title, created} of news){
+    for (const {id, icon, title, created} of news) {
         const node = `
         <div class="mdui-card mdui-m-a-3 mdui-ripple mdui-hoverable">
         <div class="mdui-card-header mdui-color-theme-900" onclick="window.location.href = 'post.html?id=${id}'">
             <img class="mdui-card-header-avatar" src="${icon}" alt="icon">
-            <div class="mdui-card-header-title">${title}/div> <!--標題-->
+            <div class="mdui-card-header-title">${title}</div> <!--標題-->
             <div class="mdui-card-header-subtitle">日期: ${created}</div> <!--副標題-->
         </div>
         </div>
