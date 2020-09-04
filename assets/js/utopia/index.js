5f8dcda37d01a0071da55e7eaec2e8563e3ebcde
EasonChu7 <59382116+EasonChu7@users.noreply.github.com>
2020-09-05 02:37:53 +0800

    aa

--- assets/js/utopia/index.js
+++ assets/js/utopia/index.js
@@ -1,199 +1,114 @@
 'use strict';
 
-const cache = {
-    uuid: undefined,
-    badge: undefined
+console.log('index.js loaded')
+
+const $ = mdui.$;
+
+const pageSettings = {
+    enableCaching: false
 }
 
-async function checkIfAdmin(){
-    if (!sessionManager.token) {
-        window.location.href = homeUrl.concat('login.html')
-    }
-    await validate(sessionManager.token).then(({res,xhr})=>{
-        if (xhr.status !== 200){
-            window.location.href = homeUrl.concat('login.html')
-            return
-        }
-        sessionManager.token = res.token
-        if (window.location.href === homeUrl.concat('utopia.html')) return
-        if (!res.user.admin){
-            alert('you are not admin!')
-            window.location.href = homeUrl.concat('utopia.html')
-        }
-    }).catch(err => {
-        console.error(err)
-        window.location.href = homeUrl.concat('login.html')
-    })
-}
+const api = "https://cloudheavenapi.iw.gy"
 
-if (sessionManager.token === undefined) {
-    document.location.href = homeUrl.concat('login.html')
-} else {
-    validate(sessionManager.token).then(({res, xhr})=>{
-        if (xhr.status === 200){
-            sessionManager.token = res.token
+$.ajaxSetup({
+    global: true,
+    method: 'POST',
+    async: true,
+    url: api,
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    contentType: 'application/json',
+    dataType: 'json',
+    processData: false,
+    xhrFields: {
+        withCredentials: true
+    },
+    timeout: 15000
+});
 
-            if (res.user.admin){
-                console.debug('user is admin')
-                const node = `
-                    <a class="mdui-list-item mdui-ripple" id="badge">
-                        <i class="mdui-list-item-icon mdui-icon material-icons">local_florist</i>
-                        <div class="mdui-list-item-content">徽章設定</div>
-                     </a>
-                     <a class="mdui-list-item mdui-ripple" id="player-setting">
-                        <i class="mdui-list-item-icon mdui-icon material-icons">person</i>
-                        <div class="mdui-list-item-content">玩家設置</div>
-                     </a>
-                `
-                $("#admin-section").replaceWith(node)
-            }
+const TOKEN_KEY = 'el.mojang.token'
 
-            $("#nav").children('a').each((_, ele) => {
-                const id = ele.getAttribute('id')
-                if (id === null) {
-                    console.debug('no id within element, won\'t change pages');
-                    return
+const ajax = async function (options){
+    return new Promise((resolve, reject) => {
+        return $.ajax({
+            ...options,
+            data: JSON.stringify(options.data || {}),
+            complete: (xhr, status) => {
+                if (status === 'error'){
+                    reject(xhr)
+                } else {
+                    const res = xhr.response ? JSON.parse(xhr.response) : undefined
+                    resolve({res, xhr})
                 }
-                if (ele.classList.contains('not-page')){
-                    console.debug(id+' has not-page, skipped');
-                    return
-                }
-                ele.addEventListener('click', () => changePage(id).catch(console.error))
-            })
-
-            updateServerCount()
-
-            return changePage('news')
-        }else{
-            alert(res)
-            alert(xhr)
-            console.warn(res)
-            console.warn(xhr)
-        }
-    }).catch(err => {
-        if (err.response){
-            console.warn(err.response)
-        }else{
-            console.error(err)
-            mdui.snackbar(err)
-        }
-        document.location.href = homeUrl.concat('login.html')
-    })
-}
-
-addPage('news', './assets/pages/news.html', true)
-addPage('about', './assets/pages/about.html')
-addPage('rule', './assets/pages/rules.html')
-addPage('intro', './assets/pages/intro.html')
-addPage('chat', './assets/pages/chat.html', true)
-addPage('player-list', './assets/pages/player-list.html', true)
-addPage('account', './assets/pages/account.html', true)
-addPage('player', './assets/pages/player.html', true)
-addPage('badge', './assets/pages/badge.html', true)
-addPage('player-setting', './assets/pages/player-setting.html', true)
-
-function userPage(uuid) {
-    cache.uuid = uuid
-    changePage('player')
-        .then(() => cache.uuid = undefined)
-        .catch(err => mdui.snackbar(err).open())
-}
-
-const drawer = new mdui.Drawer('#drawer', {swipe: true})
-
-$('#toggle-drawer').on('click', () => drawer.toggle())
-
-$("#change-color").children('button').each((_, ele) => {
-    const color = ele.getAttribute('id')
-    if (color === undefined) {
-        console.debug('no id within element, won\'t change theme');
-        return
-    }
-    ele.addEventListener('click', () => setTheme(color))
-})
-
-function handleErrorAlert(err) {
-    if (err.response){
-        console.warn(err.response)
-        const res = JSON.parse(err.response)
-        let data
-        if (res.error){
-            if (res.errorMessage === 'Invalid token'){
-                unlockOverlay()
-                mdui.dialog({
-                    title: '無效的 session Token',
-                    content: '你需要重新登入以確保順利存取資料。',
-                    buttons: [
-                        {
-                            text: '登出',
-                            onClick: () => {
-                                signOut(sessionManager.token).catch(console.error)
-                            }
-                        },
-                        {
-                            text: '取消'
-                        }
-                    ]
-                })
             }
-            data = res
-        }else{
-            data = {
-                error: res.title,
-                errorMessage: JSON.stringify(res.errors)
-            }
-        }
-        if (drawer.isDesktop()){
-            const node = alertNode(data)
-            $('#alert').replaceWith(node)
-        }else{
-            mdui.alert(data.errorMessage, data.error)
-        }
-    }else{
-        console.warn(err)
-        mdui.snackbar(err?.message || 'ERROR').open()
-    }
+        }).catch(reject)
+    })
 }
 
-$("#logout-Btn").one('click', (_) => {
-    const token = sessionManager.token
-    if (token === undefined){
-        console.warn('unknown token, back to login')
-        window.location.href = homeUrl.concat('/login.html')
-        return
-    }
-    const logoutBtn = $("#logout-btn")
-    if (isLoading(logoutBtn)){
-        return;
-    }
-    lockOverlay()
-    signOut(token).then(({_, xhr}) => {
-        console.debug(`status response: ${xhr.status}`)
-        console.debug('logout successful')
-    }).catch(xhr => {
-        console.error(xhr)
-    }).finally(() => {
-        unlockOverlay()
-        sessionManager.remove()
-        window.location.href = homeUrl.concat('/login.html')
-    })
-})
+Object.prototype.also = callback => callback(this)
 
-function lockOverlay(){
-    $.showOverlay(3000000)
-    $('body').css({
-        'pointer-events': 'none'
-    })
-    mdui.mutation()
-
+const alertNode = function (res, id = 'alert') {
+    return `
+        <div id="${id}" class="mdui-card mdui-color-red mdui-text-color-white">
+            <div class="mdui-card-primary">
+                <div class="mdui-card-primary-subtitle">
+                    <span class="mdui-text-color-white">${res.error}: ${res.errorMessage}</span>
+                </div>
+            </div>
+        </div>
+        `
 }
 
-function unlockOverlay(){
-    $.hideOverlay(true)
-    $('body').css({
-        'pointer-events': 'auto'
-    })
-    if (!drawer.isDesktop() && drawer.isOpen()) drawer.close()
+const setLoading = function (e, loading){
+    e.prop('disabled', loading)
+    if (loading) {
+        e.append('<div id="loading" class="mdui-progress-indeterminate"></div>')
+    }else {
+        $('.mdui-progress-indeterminate').remove('#loading')
+    }
     mdui.mutation()
 }
 
+const isLoading = function (e) {
+    return e.prop('disabled')
+}
+
+const homeUrl = window.location.origin.concat('/')
+
+const changeTheme = (oldColor, color) => {
+    const body =  $("body")
+    const oldColorCls = `mdui-theme-primary-${oldColor}`
+    const colorCls = `mdui-theme-primary-${color}`
+    if (body.hasClass(oldColorCls)) {
+        body.removeClass(oldColorCls)
+    }
+    body.addClass(colorCls)
+}
+
+const isDesktop = () => window.innerWidth > 600
+
+const setTheme = (color) => {
+    const oldColor = window.localStorage.getItem('el.theme.color') || 'indigo'
+    window.localStorage.setItem('el.theme.color', color)
+    changeTheme(oldColor, color)
+}
+
+const oldColor = window.localStorage.getItem('el.theme.color') || 'indigo'
+changeTheme(oldColor, oldColor)
+
+
+const sessionManager = {
+    set token(data) {
+        if (!data.accessToken || !data.clientToken){
+            console.error('cannot set token with '+JSON.stringify(data)+', pattern not match')
+        }
+        window.sessionStorage.setItem(TOKEN_KEY, btoa(JSON.stringify(data)))
+    },
+    get token() {
+        const data = window.sessionStorage.getItem(TOKEN_KEY)
+        if (data === null || data === undefined) return undefined
+        return JSON.parse(atob(data));
+    },
+    remove: () => window.sessionStorage.removeItem(TOKEN_KEY)
+}
